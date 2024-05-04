7392261ec3adeb9d297a325f4afc299ecc11578b
EvelynJ <50031451+jiangsizhu1201@users.noreply.github.com>
2024-05-04 11:25:48 -0400

    Update functions_DEM.R

--- functions_DEM.R
+++ functions_DEM.R
@@ -14,9 +14,9 @@ visual_champ_DEM <- function(myLoad,myDMP,group_list,pro='test'){
     df$v= -log10(P.Value) #df新增加一列'v',值为-log10(P.Value)
     ggscatter(df, x = "logFC", y = "v",size=0.5)
     
-    df$g=ifelse(df$P.Value>0.05,'stable', #if 判断：如果这一基因的P.Value>0.01，则为stable基因
-                ifelse( df$logFC >0.5,'up', #接上句else 否则：接下来开始判断那些P.Value<0.01的基因，再if 判断：如果logFC >1.5,则为up（上调）基因
-                        ifelse( df$logFC < -0.5,'down','stable') )#接上句else 否则：接下来开始判断那些logFC <1.5 的基因，再if 判断：如果logFC <1.5，则为down（下调）基因，否则为stable基因
+    df$g=ifelse(df$P.Value>0.05,'stable', #if P.Value>0.01，stable gene
+                ifelse( df$logFC >0.5,'up', 
+                        ifelse( df$logFC < -0.5,'down','stable') )
     )
     table(df$g)
     df$name=rownames(df)
@@ -46,20 +46,20 @@ visual_champ_DEM <- function(myLoad,myDMP,group_list,pro='test'){
     dat[1:4,1:4]
     table(group_list)
     deg=champDiff
-    x=deg$logFC #deg取logFC这列并将其重新赋值给x
-    names(x)=rownames(deg) #deg取probe_id这列，并将其作为名字给x
-    cg=c(names(head(sort(x),100)),#对x进行从小到大排列，取前100及后100，并取其对应的探针名，作为向量赋值给cg
+    x=deg$logFC 
+    names(x)=rownames(deg) 
+    cg=c(names(head(sort(x),100)),
          names(tail(sort(x),100)))
     library(pheatmap)
-    pheatmap(dat[cg,],show_colnames =F,show_rownames = F) #对dat按照cg取行，所得到的矩阵来画热图
-    n=t(scale(t(dat[cg,])))#通过“scale”对log-ratio数值进行归一化，现在的dat是行名为探针，列名为样本名，由于scale这个函数应用在不同组数据间存在差异时，需要行名为样本，因此需要用t(dat[cg,])来转换，最后再转换回来
+    pheatmap(dat[cg,],show_colnames =F,show_rownames = F) 
+    n=t(scale(t(dat[cg,])))
     
     n[n>2]=2
     n[n< -2]= -2
     n[1:4,1:4]
     pheatmap(n,show_colnames =F,show_rownames = F)
     ac=data.frame(group=group_list)
-    rownames(ac)=colnames(n) #将ac的行名也就分组信息（是‘no TNBC’还是‘TNBC’）给到n的列名，即热图中位于上方的分组信息
+    rownames(ac)=colnames(n) 
     pheatmap(n,show_colnames =F,
              show_rownames = F,
              cluster_cols = T, 
